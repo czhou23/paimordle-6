ffac5dffdef9becc46a1186fd99e3525bccc0e68
czhou23 <47546126+czhou23@users.noreply.github.com>
2022-03-08 22:51:20 -0500

    Update extreme.ts

--- src/lib/extreme.ts
+++ src/lib/extreme.ts
@@ -1,1 +1,26 @@
-import{ solution as s } from './words'
+import { solution as s, extremeSolution } from './words'
+import { useState } from 'react'
+var solu = solution
+export const [isHardMode, setIsHardMode] = useState(
+  localStorage.getItem('gameMode')
+    ? localStorage.getItem('gameMode') === 'hard'
+    : false
+)
+if(isHardMode){
+  solu = extremeSolution
+}
+export const handleHardMode = (isHard: boolean) => {
+    if (guesses.length === 0 || isGameWon || isGameLost) {
+      setIsHardMode(isHard)
+      if(isHard){
+        solu = extremeSolution
+      }
+      else{
+        solu = solution
+      }
+      localStorage.setItem('gameMode', isHard ? 'hard' : 'normal')
+    } else {
+      showErrorAlert(HARD_MODE_ALERT_MESSAGE)
+    }
+  }
+export {solu as solution}
